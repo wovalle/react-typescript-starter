a2f6a6afeba53be7478a621b940be6fb082f659b
Willy Ovalle <willyovalle16@gmail.com>
2017-09-18 16:57:55 -0400

    Enhancement: more webpack magic && adding build command

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,11 +1,5 @@
 /** Global definitions for developement **/
 
-// for style loader
-declare module '*.css' {
-  const styles: any;
-  export = styles;
-}
-
 // for redux devtools extension
 declare interface Window {
   devToolsExtension?(): (args?: any) => any;
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,13 +3,19 @@ import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import autoprefixer from 'autoprefixer';
+import WebpackMd5Hash from 'webpack-md5-hash';
 
 // variables
+const GLOBALS = {
+  'process.env.NODE_ENV': JSON.stringify('production'),
+  __DEV__: false,
+};
+
 const sourcePath = path.join(__dirname, './src');
 const outPath = path.join(__dirname, './dist');
 
 export default {
-  devtool: 'eval-source-map', // more info:https://webpack.js.org/guides/development/#using-source-maps and https://webpack.js.org/configuration/devtool/
+  devtool: 'source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
   resolve: {
     extensions: ['*', '.js', '.ts', '.tsx'],
     // Fix webpack's default behavior to not load packages with jsnext:main module
@@ -17,29 +23,57 @@ export default {
     mainFields: ['main']
   },
   context: sourcePath,
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-hot-middleware/client?reload=true',
-    path.resolve(__dirname, 'src/index.tsx'),
-  ],
+  entry: {
+    main: './index.tsx',
+    vendor: [
+      'react',
+      'react-dom',
+      'react-redux',
+      'react-router',
+      'redux'
+    ]
+  },
   output: {
     path: outPath,
     publicPath: '/',
-    filename: 'bundle.js',
+    filename: '[name].[chunkhash].js'
   },
   target: 'web',
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
+    // Hash the files using MD5 so that their names change when the content changes.
+    new WebpackMd5Hash(),
+
+    // Tells React to build in prod mode. https://facebook.github.io/react/downloads.html
+    new webpack.DefinePlugin(GLOBALS),
+    // Generate an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
-      filename: 'vendor.bundle.js',
+      filename: '[name].[chunkhash].js',
       minChunks: Infinity
     }),
-    new webpack.optimize.AggressiveMergingPlugin(),
+
     new HtmlWebpackPlugin({
-      template: 'index.html'
+      template: 'index.html',
+      favicon: './favicon.ico',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true,
     }),
+    // Minify JS
+    new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
+
     new webpack.LoaderOptionsPlugin({
       minimize: false,
       debug: true,
@@ -55,7 +89,7 @@ export default {
   ],
   module: {
     rules: [
-      {test: /\.tsx?$/, loaders: ['react-hot-loader/webpack', 'awesome-typescript-loader']},
+      {test: /\.tsx?$/, loaders: 'awesome-typescript-loader'},
       {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file-loader'},
       {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff'},
       {test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
